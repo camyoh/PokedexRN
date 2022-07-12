7b46f46ff32f8a62b77b957f709f506c5fe511e0
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 07:41:42 -0500

    usando useRef

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { pokemonApi } from '../api/pokemonApi';
 
 export const usePokemonPaginated = () => {
     
-    const url = "https://pokeapi.co/api/v2/pokemon?limit=40";
+    const nextPageUrl = useRef("https://pokeapi.co/api/v2/pokemon?limit=40")
 
     const loadPokemons =async () => {
-        const resp = await pokemonApi.get(url);
+        const resp = await pokemonApi.get(nextPageUrl.current);
         console.log(resp.data);
         
     }
