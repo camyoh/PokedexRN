56a585f936ef15cee7bd372a9024b71f91f7f5cf
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 16:29:44 -0500

    agregando titulos de pero y los sprites y abilities

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { PokemonFull } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
     pokemon: PokemonFull;
@@ -34,12 +35,58 @@ export const PokemonDetails = ( {pokemon}: Props ) => {
                         ))
                     }
                 </View>
+
+                <Text style={styles.title}>Weight</Text>
+                <Text style={styles.regularText}>{pokemon.weight} Lb</Text>
             </View>
 
             <View
                 style={styles.container}
             >
                 <Text style={styles.title}>Sprites</Text>
+            </View>
+
+            <ScrollView
+                horizontal = {true}
+                showsHorizontalScrollIndicator = {false}
+            >
+                <FadeInImage 
+                    uri={pokemon.sprites.front_default}
+                    style={styles.basicSprite}
+                />
+                <FadeInImage 
+                    uri={pokemon.sprites.back_default}
+                    style={styles.basicSprite}
+                />
+                <FadeInImage 
+                    uri={pokemon.sprites.front_shiny}
+                    style={styles.basicSprite}
+                />
+                <FadeInImage 
+                    uri={pokemon.sprites.back_shiny}
+                    style={styles.basicSprite}
+                />
+            </ScrollView>
+
+            <View
+                style={styles.container}
+            >
+                <Text style={styles.title}>Abilities</Text>
+                <View style={{flexDirection: 'row'}}>
+                    {
+                        pokemon.abilities.map( ({ability}) => (
+                            <Text
+                                style={{
+                                    ...styles.regularText,
+                                    marginRight: 10,
+                                }}
+                                key={ability.name}
+                            >
+                                { ability.name }
+                            </Text>
+                        ))
+                    }
+                </View>
             </View>
 
         </ScrollView>
@@ -57,5 +104,9 @@ const styles = StyleSheet.create({
     },
     regularText: {
         fontSize: 19,
+    },
+    basicSprite: {
+        width: 100,
+        height: 100,
     }
 });
