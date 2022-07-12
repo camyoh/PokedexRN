c5a152021629d5b6d833a2d5150c7caa49313752
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 12:37:42 -0500

    creando los parámetros q se pasan desde el Stack Navigator, y creando la interface que hereda de stackSreenProps

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,7 +1,11 @@
+import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
 import { Text, View } from 'react-native'
+import { RootStackParams } from '../navigator/Navigator'
 
-export const PokemonScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'>{};
+
+export const PokemonScreen = (props: Props) => {
     return (
         <View>
             <Text>PokemonScreen</Text>
