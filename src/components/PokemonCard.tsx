c3814c3e007a358228f02a96c6a2ab4c8ad2b2f6
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 09:03:55 -0500

    creando las tarjetas rojas para cada pk

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SimplePokemon } from '../interfaces/pokemonInterfaces'
+import { FadeInImage } from './FadeInImage';
+
+const windowWidth = Dimensions.get('window').width
 
 interface Props {
     pokemon: SimplePokemon;
 }
 
-export const PokemonCard = ( {pokemon}:Props ) => {
+export const PokemonCard = ({ pokemon }: Props) => {
     return (
-        <View>
-            <Text>{pokemon.name}</Text>
-        </View>
+        <TouchableOpacity>
+            <View style={{
+                ...styles.cardContainer,
+                width: windowWidth * 0.4,
+            }}>
+
+            </View>
+        </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        marginHorizontal: 10,
+        backgroundColor: "red",
+        height: 120,
+        width: 160,
+        marginBottom: 25,
+        borderRadius: 10,
+    }
+});
