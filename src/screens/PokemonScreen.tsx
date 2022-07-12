8c8d282753b03b1f67d2a45934cbdc567b505d13
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 13:23:13 -0500

    creando estilos para el view y redondeando el view

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,17 +1,32 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { RootStackParams } from '../navigator/Navigator'
 
-interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'>{};
+interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> { };
 
-export const PokemonScreen = ({navigation, route}: Props) => {
+export const PokemonScreen = ({ navigation, route }: Props) => {
 
-    const {simplePokemon, color} = route.params;
+    const { simplePokemon, color } = route.params;
 
     return (
-        <View>
-            <Text>{simplePokemon.name} - {color}</Text>
+        <View
+            style={{
+                ...styles.headerContainer,
+                backgroundColor: color,
+            }}
+        >
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 370,
+        zIndex: 999,
+        alignItems: 'center',
+        borderBottomLeftRadius: 1000,
+        borderBottomRightRadius: 1000,
+    }
+})
