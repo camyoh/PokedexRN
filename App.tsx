519fe8acadea065f54a5bb37bd87423a0e19d27f
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 06:59:06 -0500

    haciendo la prueba de los iconos, importanto el Icon y al pararnos encima de la ruta presionar ctrl+. para instalar la dependencia que hace falta

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const App = () => {
   return (
     <>
       <Text>App Screen</Text>
+
+      <Icon
+        name = "add-circle-outline"
+        color = "red"
+        size={ 100 }
+      />
     </>
   )
 }
