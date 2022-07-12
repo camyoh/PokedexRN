561917fa0afa51252f95a30b635cc2cf10dfde80
andresMendieta <carlos.camyoh@gmail.com>
2022-07-12 07:17:16 -0500

    instalando navigation stack y gesture handler, mostrando el navigator en el app, creando el navigator con las dos vistas de la app.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { Text } from 'react-native'
+import { Navigator } from './src/navigator/Navigator'
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Text>App Screen</Text>
+            <Navigator />
         </NavigationContainer>
     )
 }
